3a3fff6301f485432a954b685621a838ae2e3dc1
Adrian Osica <s23615@pjwstk.edu.pl>
2024-03-17 13:12:50 +0100

    Project init

---
+++ Assets/Scripts/HealthUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health = 100; // Domyślne życie gracza
+    public TextMeshProUGUI healthText; // Referencja do komponentu tekstowego
+
+    void Update()
+    {
+        healthText.text = "Życie: " + health.ToString(); // Aktualizacja tekstu życia
+    }
+}
